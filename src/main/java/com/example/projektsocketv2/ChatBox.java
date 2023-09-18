870654529bc45e9423bada3270e544bb23fce637
Naren's bumcrack <luhavenkvist@yahoo.dk>
2023-09-18 13:02:25 +0200

    Scene ændring

--- src/main/java/com/example/projektsocketv2/ChatBox.java
+++ src/main/java/com/example/projektsocketv2/ChatBox.java
@@ -2,48 +2,33 @@ package com.example.projektsocketv2;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class ChatBox extends Application {
-    private Stage primaryStage;
-
-    public void setPrimaryStage(Stage primaryStage) {
-        this.primaryStage = primaryStage;
+    public static void main(String[] args) {
+        launch(args);
     }
 
-    public Stage getPrimaryStage() {
-        return primaryStage;
-    }
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ChatBox.class.getResource("login-box.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Login-Box");
-        stage.setScene(scene);
-        stage.show();
-
-        setPrimaryStage(stage);
-
-        System.out.println(getPrimaryStage());
-    }
-
-    /*public void switchToChatBox() {
+    public void start(Stage primaryStage) {
         try {
-            Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("chat-box.fxml"))));
-            primaryStage.setScene(scene);
+            // Load login-box.fxml
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("login-box.fxml"));
+            Parent root = loader.load();
+
+            // Set the controller for login-box
+            LoginController loginController = loader.getController();
+            loginController.setPrimaryStage(primaryStage);
+
+            primaryStage.setTitle("Scene 1");
+            primaryStage.setScene(new Scene(root));
             primaryStage.show();
-            System.out.println(primaryStage);
-            System.out.println(getPrimaryStage());
         } catch (IOException e) {
             e.printStackTrace();
         }
-    } */
-
-    public static void main(String[] args) {
-        launch();
     }
 }
--- src/main/java/com/example/projektsocketv2/ChatController.java
+++ src/main/java/com/example/projektsocketv2/ChatController.java
@@ -1,29 +1,23 @@
 package com.example.projektsocketv2;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-
-import java.util.ArrayList;
 
 public class ChatController {
     @FXML private TextField chatInput;
     @FXML private TextArea chatBox;
     @FXML private Button sendKnap;
     @FXML private Button lukKnap;
-    public String name = LoginController.getName();
 
-    @FXML
-    protected void onButtonSend() {
+    public void onButtonSend() {
         String input = chatInput.getText();
-        chatBox.appendText( name + "> " + input + "\n");
-        chatInput.setText("");
+        chatBox.appendText(input + "\n");
+        chatInput.clear();
     }
 
-    @FXML
-    protected void onButtonLuk() {
-        Platform.exit();
+    public void onButtonLuk() {
+        System.exit(0);
     }
 }
