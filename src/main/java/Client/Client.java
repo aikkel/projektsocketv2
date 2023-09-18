8ca1fe34157b49039a9458cb01cf1b23c080c6d0
Naren's bumcrack <luhavenkvist@yahoo.dk>
2023-09-18 12:44:55 +0200

    Client og server klasserne

---
+++ src/main/java/Client/Client.java
@@ -0,0 +1,31 @@
+package Client;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) {
+        String serverAddress = "127.0.0.1"; //Replace with the actual IP address or hostname of the server
+        int serverPort = 8080; //Replace with the port number the server is listening on
+
+        try {
+            Socket socket = new Socket(serverAddress, serverPort);
+
+            //Create input and output streams for reading and writing data.
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            //Send data to the server
+            out.println("Hello, Server!");
+
+            //Receive data from the server
+            String response = in.readLine();
+            System.out.println("Server Response: " + response);
+
+            //Close the socket when done.
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Client/Server.java
@@ -0,0 +1,38 @@
+package Client;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) {
+        int serverPort = 8080; //Port number the server will listen on
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(serverPort);
+            System.out.println("Server is listening on port " + serverPort);
+
+            while (true) {
+                //Accept incoming client connections
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+
+                //Create input and output streams for reading and writing data
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                //Read data from the client
+                String clientMessage = in.readLine();
+                System.out.println("Received from client: " + clientMessage);
+
+                //Send a response back to the client
+                out.println("Hello, Client!");
+
+                //Close the client socket
+                clientSocket.close();
+                System.out.println("Client disconnected");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
